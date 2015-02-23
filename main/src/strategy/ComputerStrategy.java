e32dc69603432167939064477b5edbfcb1bb4ab1
trampboy <jhg19900321@163.com>
2015-02-23 14:24:54 +0800

    commit decorator pattern

---
+++ main/src/strategy/ComputerStrategy.java
@@ -0,0 +1,11 @@
+package strategy;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+/**
+ * Created by TrampBoy on 2015/2/23.
+ */
+public abstract class ComputerStrategy {
+    public abstract int computer(int[] arrayList);
+}
