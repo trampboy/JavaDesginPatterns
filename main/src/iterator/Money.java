9d788529ed36866bea57a708a66477bb67627150
trampboy <jhg19900321@163.com>
2015-02-23 22:17:37 +0800

    commit iterator pattern

---
+++ main/src/iterator/Money.java
@@ -0,0 +1,22 @@
+package iterator;
+
+/**
+ * Created by TrampBoy on 2015/2/23.
+ */
+public class Money{
+    int value;
+    boolean isTrue = false;
+
+    Money(int value, boolean isTrue) {
+        this.value = value;
+        this.isTrue = isTrue;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public boolean isTrue() {
+        return isTrue;
+    }
+}
