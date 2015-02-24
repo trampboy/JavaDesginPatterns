cced82138144036be9ed8f1c7ec36c5ef0bd91db
trampboy <jhg19900321@163.com>
2015-02-24 23:38:11 +0800

    commit mediator pattern

---
+++ main/src/mediator/ConcreteMediator.java
@@ -0,0 +1,45 @@
+package mediator;
+
+import java.util.ArrayList;
+
+/**
+ * Created by TrampBoy on 2015/2/24.
+ */
+public class ConcreteMediator {
+    ArrayList<Lender> lenders;
+    ArrayList<Lessor> lessors;
+
+    public ConcreteMediator() {
+        lenders = new ArrayList<Lender>();
+        lessors = new ArrayList<Lessor>();
+    }
+
+    public void registerLender(Lender lender){
+        lenders.add(lender);
+    }
+
+    public void registerLessor(Lessor lessor){
+        lessors.add(lessor);
+    }
+
+    public void deliverMess(User user, String mess){
+        if (user instanceof Lender) {
+            lessorReceiverMess(mess);
+        } else if (user instanceof Lessor) {
+            lenderReceiverMess(mess);
+        }
+    }
+
+    private void lenderReceiverMess(String mess) {
+        for (Lender lender : lenders) {
+            lender.receiverMess(mess);
+        }
+    }
+
+    private void lessorReceiverMess(String mess) {
+        for (Lessor lessor : lessors ) {
+            lessor.receiverMess(mess);
+        }
+    }
+
+}
